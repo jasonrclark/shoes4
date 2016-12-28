69b90a0d97a67c0c02f6035ed3cdceb8c0f201a3
Jason R. Clark <jason@jasonrclark.net>
2016-12-28 14:27:00 -0800

    wip

--- shoes-swt/lib/shoes/swt/generate_backend.rb
+++ shoes-swt/lib/shoes/swt/generate_backend.rb
@@ -3,6 +3,28 @@ require 'rbconfig'
 class Shoes
   module SelectedBackend
     class << self
+      def validate
+        if RUBY_ENGINE != "jruby"
+          puts <<-EOS
+
+*******************************************************************************
+The SWT backend for Shoes requires a 9.X version of JRuby.
+
+You appear to be running the following Ruby instead:
+
+  #{RUBY_DESCRIPTION}
+
+Please download JRuby from http://jruby.org (or your favorite Ruby version
+manager), reinstall the `shoes` gem, and try again.
+
+Sorry for the inconvenience!
+*******************************************************************************
+
+EOS
+          exit 1
+        end
+      end
+
       def generate(path)
         options = "-J-XstartOnFirstThread" if RbConfig::CONFIG["host_os"] =~ /darwin/
 
